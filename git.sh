750d974079a67f26fc73bf51a5402c73390dc49d
mohamedyacine.jlassi <mohamedyacine.jlassi@allence-tunisie.com>
2017-01-01 14:00:00 +0530

    extra code removed

--- git.sh
+++ git.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-list=("added index file","updated readme file", "did some extra calculations","deleted media files","extra code removed")
-for i in {1..12}
+
+list=("added index file" "updated readme file" "did some extra calculations" "deleted media files" "extra code removed")
+
+# Loop through each month (1 to 12)
+for month in {1..12}
 do
-	touch temp$i.txt
-	git add -A
-	a="Sun Mar "
-	b=$i
-	c=" 14:00 2017 +0530"
-	export GIT_AUTHOR_DATE=$a$b$c
-	export GIT_COMMITTER_DATE=$a$b$c
-	git commit -am "`echo ${list[$RANDOM % ${#list[@]} ]}`"
-	rm temp$i.txt
-	export GIT_AUTHOR_DATE=$a$b$c
-	export GIT_COMMITTER_DATE=$a$b$c
-	export GIT_AUTHOR_NAME="YACINEgomycode"
-	export GIT_AUTHOR_EMAIL="jlassi_yassine@live.fr"
-	export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
-	export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
-	git commit -m "Addes some patches"
-	git commit -am "`echo ${list[$RANDOM % ${#list[@]} ]}`"
-	git push --force
+  # Loop through 20 random days in the current month
+  for ((day=1; day<=20; day++))
+  do
+    # Generate a random number of commits between 5 and 45
+    num_commits=$((5 + RANDOM % 41))
+
+    # Set the date to be the 1st of the current month in the year 2017
+    export GIT_AUTHOR_DATE="2017-$month-01 14:00 +0530"
+    export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"
+
+    # Loop to generate random commits for the current day
+    for ((commit=1; commit<=num_commits; commit++))
+    do
+      touch temp${month}_${day}_${commit}.txt
+      git add -A
+      git commit -am "${list[$RANDOM % ${#list[@]}]}"
+      rm temp${month}_${day}_${commit}.txt
+    done
+
+    # Push the commits for the current day
+    git push
+  done
 done
+
