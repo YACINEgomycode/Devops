9cb8b735566b2dc90c33c16301dddf24be7495d8
YACINEgomycode <jlassi_yassine@live.fr>
2017-01-01 14:00:00 +0530

    updated readme file

--- git.sh
+++ git.sh
@@ -3,13 +3,13 @@
 list=("added index file" "updated readme file" "did some extra calculations" "deleted media files" "extra code removed")
 
 # Loop through each month (1 to 12)
-for month in {1..3}
+for month in {1..12}
 do
   # Loop through 20 random days in the current month
   for ((day=1; day<=20; day++))
   do
-    # Generate a random number of commits between 5 and 45
-    num_commits=$((5 + RANDOM % 41))
+    # Generate a random number of commits between 5 and 41
+    num_commits=$((5 + RANDOM % 37))
 
     # Set the date to be the 1st of the current month in the year 2017
     export GIT_AUTHOR_DATE="2017-$month-01 14:00 +0530"
@@ -18,6 +18,7 @@ do
     export GIT_AUTHOR_EMAIL="jlassi_yassine@live.fr"
     export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
     export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+
     # Loop to generate random commits for the current day
     for ((commit=1; commit<=num_commits; commit++))
     do
@@ -31,7 +32,4 @@ do
     git push --force
   done
 done
-
-
-
 
