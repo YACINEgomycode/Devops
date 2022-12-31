1a473da857afcf25a795b62301fbaf7169fda7ae
YACINEgomycode <jlassi_yassine@live.fr>
2023-01-01 00:00:22 +0530

    extra code removed

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@
 list=("added index file" "updated readme file" "did some extra calculations" "deleted media files" "extra code removed")
 
 # Loop through each month (1 to 12)
-for month in {1..12}
+for month in {1..10}
 do
   # Create an array of 20 random unique day numbers
   random_days=($(shuf -i 1-31 -n 17 | tr ' ' '\n' | sort -n))
@@ -14,7 +14,7 @@ do
     num_commits=$((5 + RANDOM % 37))
 
     # Set the date to be the selected day of the current month in the year 2017
-    export GIT_AUTHOR_DATE="2018-$month-$day $(shuf -i 0-23 -n 1):$(shuf -i 0-59 -n 1):$(shuf -i 0-59 -n 1) +0530"
+    export GIT_AUTHOR_DATE="2023-$month-$day $(shuf -i 0-23 -n 1):$(shuf -i 0-59 -n 1):$(shuf -i 0-59 -n 1) +0530"
     export GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE"
     export GIT_AUTHOR_NAME="YACINEgomycode"
     export GIT_AUTHOR_EMAIL="jlassi_yassine@live.fr"
